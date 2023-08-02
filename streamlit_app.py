c4ad58cc6bfb29a75315a976b20ccfff904f1cd7
Spaicol1 <139074026+Spaicol1@users.noreply.github.com>
2023-08-02 22:46:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,115 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
+# Load the Excel files
+naics_definitions = pd.read_excel('C:/Users/SONY/Desktop/NAICS_definitions.xlsx')
+ctyname_definitions = pd.read_excel('C:/Users/SONY/Desktop/Geographical Designations.xlsx')
 
-# Function to filter data based on user selection
-def filter_data(data, column, min_val, max_val):
-    return data[(data[column] >= min_val) & (data[column] <= max_val)]
+# List of file paths and corresponding years
+file_years = [
+    ('C:/Users/SONY/Desktop/CompleteCounty2021.txt', 2021),
+    ('C:/Users/SONY/Desktop/CompleteCounty2020.txt', 2020),
+    ('C:/Users/SONY/Desktop/CompleteCounty2019.txt', 2019),
+    ('C:/Users/SONY/Desktop/CompleteCounty2018.txt', 2018),
+    ('C:/Users/SONY/Desktop/CompleteCounty2017.txt', 2017),
+    # Add more file paths and years as needed
+]
+
+# Function to filter data for specific industries related to selected NAICS codes
+def apply_filters(selected_naics):
+    filtered_data = pd.DataFrame()
+    naics_values = set()
+    est_sum_data_dict = {}
+    fipstate_data = {}
+
+    for file_path, year in file_years:
+        # Load the TXT data into a pandas DataFrame
+        data = pd.read_csv(file_path, delimiter=',')
+        # Assign year to the data
+        data['year'] = year
+        # Collect unique NAICS values in the loaded data
+        naics_values.update(naics_definitions['NAICS'].astype(str).unique())
+        # Filter the data based on selected NAICS codes
+        for naics_code in selected_naics:
+            filtered_data = pd.concat([filtered_data, data[data['naics'].astype(str) == naics_code][['naics', 'fipstate', 'est', 'fipscty', 'year']]])
+
+    # Merge the filtered_data DataFrame with the ctyname_definitions DataFrame based on 'fipstate' and 'fipscty'
+    merged_data = pd.merge(filtered_data, ctyname_definitions, how='left', left_on=['fipstate', 'fipscty'], right_on=['st', 'cty'])
+
+    if not merged_data.empty:
+        # Drop duplicates based on 'fipstate', 'fipscty', and 'year' columns
+        merged_data = merged_data.drop_duplicates(subset=['fipstate', 'fipscty', 'year'])
+        # Sort the merged data by year
+        merged_data = merged_data.sort_values('year')
+
+        # Match fipstate and fipscty and store est values per year with corresponding ctyname
+        fipstate_fipscty = merged_data.groupby(['fipstate', 'fipscty'])
+        filtered_data_list = []
+
+        for (fipstate, fipscty), group_data in fipstate_fipscty:
+            city_name = group_data['ctyname'].iloc[0]
+            est_values = []
+            for year, est in zip(group_data['year'], group_data['est']):
+                est_values.append((year, est))
+            filtered_data_list.append((fipstate, fipscty, city_name, est_values))
+
+        # Calculate the total sum per fipstate within a year
+        for year, year_data in merged_data.groupby('year'):
+            est_sum_data_dict = year_data.groupby(['year', 'fipstate'])['est'].sum().reset_index()
+    
+            for fipstate, total in zip(est_sum_data_dict['fipstate'], est_sum_data_dict['est']):
+                if fipstate not in fipstate_data:
+                    fipstate_data[fipstate] = []
+                fipstate_data[fipstate].append((year, total))
+
+    return fipstate_data, filtered_data_list
 
 def main():
-    st.title('Line Chart with Selection Bars')
+    st.title('NAICS Checklist and Line Charts')
 
-    # Sidebar selection bars
-    selected_column = st.sidebar.selectbox('Select Column to Filter', chart_data.columns)
-    min_value = st.sidebar.slider('Select Min Value', float(chart_data[selected_column].min()), float(chart_data[selected_column].max()), float(chart_data[selected_column].min()))
-    max_value = st.sidebar.slider('Select Max Value', float(chart_data[selected_column].min()), float(chart_data[selected_column].max()), float(chart_data[selected_column].max()))
+    # List to store the selected NAICS codes
+    selected_naics = []
 
-    # Filter data based on user selection
-    filtered_data = filter_data(chart_data, selected_column, min_value, max_value)
+    st.sidebar.header('Select NAICS Codes:')
 
-    # Display the filtered data
-    st.write(filtered_data)
+    # Create a checkbox for each NAICS code
+    for index, row in naics_definitions.iterrows():
+        naics_code = row['NAICS']
+        naics_description = row['Description']
+        checkbox = st.sidebar.checkbox(f"{naics_code} - {naics_description}")
+        if checkbox:
+            selected_naics.append(naics_code)
 
-    # Plot the line chart for the filtered data
-    st.line_chart(filtered_data)
+    # Display the selected NAICS codes
+    if selected_naics:
+        st.sidebar.subheader('Selected NAICS Codes:')
+        st.sidebar.write(selected_naics)
+
+    # Button to apply filters and plot line charts
+    st.sidebar.subheader('Plot Line Charts')
+    plot_button = st.sidebar.button('Apply Filters and Plot Line Charts')
+
+    # If the user has selected some NAICS codes and clicked the plot button, apply filters and plot line charts
+    if selected_naics and plot_button:
+        fipstate_data, filtered_data_list = apply_filters(selected_naics)
+
+        # Plot line chart for total estimates per fipstate
+        for fipstate, data in fipstate_data.items():
+            years = np.array([year for year, _ in data])
+            totals = np.array([total for _, total in data])
+
+            st.subheader(f"Total Companies per Fipstate ({fipstate})")
+            st.line_chart(pd.DataFrame({'Year': years, 'Total Estimates': totals}))
+
+        # Plot line charts for individual ctynames
+        for fipstate, fipscty, city_name, est_values in filtered_data_list:
+            years = np.array([year for year, _ in est_values])
+            estimates = np.array([est for _, est in est_values])
+
+            st.subheader(f"Companies in {city_name} ({fipstate}-{fipscty})")
+            st.line_chart(pd.DataFrame({'Year': years, '# of Companies': estimates}))
 
 if __name__ == '__main__':
     main()
