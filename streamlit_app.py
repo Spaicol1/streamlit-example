92f293bfd00db772313d4c6f1ddf4b0987500660
Spaicol1 <139074026+Spaicol1@users.noreply.github.com>
2023-08-02 21:17:25 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,26 @@ chart_data = pd.DataFrame(
     np.random.randn(20, 3),
     columns=['a', 'b', 'c'])
 
-st.line_chart(chart_data)
+# Function to filter data based on user selection
+def filter_data(data, column, min_val, max_val):
+    return data[(data[column] >= min_val) & (data[column] <= max_val)]
+
+def main():
+    st.title('Line Chart with Selection Bars')
+
+    # Sidebar selection bars
+    selected_column = st.sidebar.selectbox('Select Column to Filter', chart_data.columns)
+    min_value = st.sidebar.slider('Select Min Value', float(chart_data[selected_column].min()), float(chart_data[selected_column].max()), float(chart_data[selected_column].min()))
+    max_value = st.sidebar.slider('Select Max Value', float(chart_data[selected_column].min()), float(chart_data[selected_column].max()), float(chart_data[selected_column].max()))
+
+    # Filter data based on user selection
+    filtered_data = filter_data(chart_data, selected_column, min_value, max_value)
+
+    # Display the filtered data
+    st.write(filtered_data)
+
+    # Plot the line chart for the filtered data
+    st.line_chart(filtered_data)
+
+if __name__ == '__main__':
+    main()
