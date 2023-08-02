04d8b4922dd2590837a8064d00759743e1366ac5
Spaicol1 <139074026+Spaicol1@users.noreply.github.com>
2023-08-02 23:22:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import os
 
-# Get the current working directory
-base_dir = os.getcwd()
-
-# Define the file paths relative to the current working directory
-naics_definitions_path = os.path.join(base_dir, 'NAICS_definitions.xlsx')
-ctyname_definitions_path = os.path.join(base_dir, 'Geographical Designations.xlsx')
-
-# Function to load NAICS definitions from the Excel file
-def load_naics_definitions():
-    return pd.read_excel(naics_definitions_path)
+def load_naics_definitions(uploaded_file):
+    return pd.read_excel(uploaded_file)
 
 # Function to filter data for specific industries related to selected NAICS codes
 def apply_filters(naics_definitions, selected_naics):
@@ -23,49 +14,53 @@ def apply_filters(naics_definitions, selected_naics):
 def main():
     st.title('NAICS Checklist and Line Charts')
 
-    # Load the NAICS definitions
-    naics_definitions = load_naics_definitions()
+    # Upload the NAICS definitions file
+    naics_definitions_file = st.file_uploader("Upload NAICS Definitions Excel file", type=["xlsx"])
 
-    # List to store the selected NAICS codes
-    selected_naics = []
+    # Load the NAICS definitions if the file is uploaded
+    if naics_definitions_file:
+        naics_definitions = load_naics_definitions(naics_definitions_file)
 
-    st.sidebar.header('Select NAICS Codes:')
+        # List to store the selected NAICS codes
+        selected_naics = []
 
-    # Create a checkbox for each NAICS code
-    for index, row in naics_definitions.iterrows():
-        naics_code = row['NAICS']
-        naics_description = row['Description']
-        checkbox = st.sidebar.checkbox(f"{naics_code} - {naics_description}")
-        if checkbox:
-            selected_naics.append(naics_code)
+        st.sidebar.header('Select NAICS Codes:')
 
-    # Display the selected NAICS codes
-    if selected_naics:
-        st.sidebar.subheader('Selected NAICS Codes:')
-        st.sidebar.write(selected_naics)
+        # Create a checkbox for each NAICS code
+        for index, row in naics_definitions.iterrows():
+            naics_code = row['NAICS']
+            naics_description = row['Description']
+            checkbox = st.sidebar.checkbox(f"{naics_code} - {naics_description}")
+            if checkbox:
+                selected_naics.append(naics_code)
 
-    # Button to apply filters and plot line charts
-    plot_button = st.sidebar.button('Apply Filters and Plot Line Charts')
+        # Display the selected NAICS codes
+        if selected_naics:
+            st.sidebar.subheader('Selected NAICS Codes:')
+            st.sidebar.write(selected_naics)
 
-    # If the user has selected some NAICS codes and clicked the plot button, apply filters and plot line charts
-    if selected_naics and plot_button:
-        fipstate_data, filtered_data_list = apply_filters(naics_definitions, selected_naics)
+        # Button to apply filters and plot line charts
+        plot_button = st.sidebar.button('Apply Filters and Plot Line Charts')
 
-        # Plot line chart for total estimates per fipstate
-        for fipstate, data in fipstate_data.items():
-            years = np.array([year for year, _ in data])
-            totals = np.array([total for _, total in data])
+        # If the user has selected some NAICS codes and clicked the plot button, apply filters and plot line charts
+        if selected_naics and plot_button:
+            fipstate_data, filtered_data_list = apply_filters(naics_definitions, selected_naics)
 
-            st.subheader(f"Total Companies per Fipstate ({fipstate})")
-            st.line_chart(pd.DataFrame({'Year': years, 'Total Estimates': totals}))
+            # Plot line chart for total estimates per fipstate
+            for fipstate, data in fipstate_data.items():
+                years = np.array([year for year, _ in data])
+                totals = np.array([total for _, total in data])
 
-        # Plot line charts for individual ctynames
-        for fipstate, fipscty, city_name, est_values in filtered_data_list:
-            years = np.array([year for year, _ in est_values])
-            estimates = np.array([est for _, est in est_values])
+                st.subheader(f"Total Companies per Fipstate ({fipstate})")
+                st.line_chart(pd.DataFrame({'Year': years, 'Total Estimates': totals}))
 
-            st.subheader(f"Companies in {city_name} ({fipstate}-{fipscty})")
-            st.line_chart(pd.DataFrame({'Year': years, '# of Companies': estimates}))
+            # Plot line charts for individual ctynames
+            for fipstate, fipscty, city_name, est_values in filtered_data_list:
+                years = np.array([year for year, _ in est_values])
+                estimates = np.array([est for _, est in est_values])
+
+                st.subheader(f"Companies in {city_name} ({fipstate}-{fipscty})")
+                st.line_chart(pd.DataFrame({'Year': years, '# of Companies': estimates}))
 
 if __name__ == '__main__':
     main()
