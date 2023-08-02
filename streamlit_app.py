bf7f77d31f0bfbfe3d779f646e0f3a87ba130b2a
Spaicol1 <139074026+Spaicol1@users.noreply.github.com>
2023-08-03 01:05:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,24 +44,37 @@ def main():
     # Add a title to the chart
     st.title("Line Chart with Hover Data Points")
 
-    # Add a sidebar for selecting different sets of data and years
-    selected_establishment_type = st.sidebar.selectbox("Select Establishment Type:", establishment_types)
-    selected_state = st.sidebar.selectbox("Select State:", states)
-    selected_county = st.sidebar.selectbox("Select County:", counties)
-    selected_naics = st.sidebar.selectbox("Select NAICs:", naics)
+    # Add a tab for the chart
+    chart_tab = st.beta_container()
+    with chart_tab:
+        # Add a sidebar for selecting different sets of data and years
+        selected_establishment_type = st.sidebar.selectbox("Select Establishment Type:", establishment_types)
+        selected_state = st.sidebar.selectbox("Select State:", states)
+        selected_county = st.sidebar.selectbox("Select County:", counties)
+        selected_naics = st.sidebar.selectbox("Select NAICs:", naics)
 
-    # Add a slider to tweak the years selected
-    start_year = st.sidebar.slider("Select start year:", min_value=min(years), max_value=max(years), value=min(years))
-    end_year = st.sidebar.slider("Select end year:", min_value=min(years), max_value=max(years), value=max(years))
+        # Add a slider to tweak the years selected
+        start_year = st.sidebar.slider("Select start year:", min_value=min(years), max_value=max(years), value=min(years))
+        end_year = st.sidebar.slider("Select end year:", min_value=min(years), max_value=max(years), value=max(years))
 
-    # Filter the data based on the selected dropdown values and years
-    filtered_data = df  # You can replace this with actual filtering logic based on the selected values and years
+        # Filter the data based on the selected dropdown values and years
+        filtered_data = df  # You can replace this with actual filtering logic based on the selected values and years
 
-    # Display the chart using Streamlit
-    st.altair_chart(chart, use_container_width=True)
+        # Display the chart using Streamlit
+        st.altair_chart(chart, use_container_width=True)
 
-    # Display the filtered data as a table
-    st.dataframe(filtered_data)
+        # Display the filtered data as a table
+        st.dataframe(filtered_data)
+
+    # Add a tab for the dataframe (table)
+    data_tab = st.beta_container()
+    with data_tab:
+        st.dataframe(df)
+
+    # Add a tab for the export options
+    export_tab = st.beta_container()
+    with export_tab:
+        st.write("Add your export options here")
 
 if __name__ == '__main__':
     main()
