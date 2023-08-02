30391c4a2d1144882493d9bfa44f46ae558c8044
Spaicol1 <139074026+Spaicol1@users.noreply.github.com>
2023-08-03 00:32:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,31 @@
-from streamlit_extras.chart_container import chart_container
+import streamlit as st
+import pandas as pd
+import altair as alt
 
-chart_data = _get_random_data()
-with chart_container(chart_data):
-    st.write("Here's a cool chart")
-    st.area_chart(chart_data)
+def main():
+    # Sample data
+    data = {
+        'x_values': [1, 2, 3, 4, 5],
+        'y_values': [10, 5, 7, 12, 8]
+    }
+    df = pd.DataFrame(data)
+
+    # Create the line chart using Altair
+    line_chart = alt.Chart(df).mark_line().encode(
+        x='x_values',
+        y='y_values'
+    )
+
+    # Add interactive data points using the `circle` mark
+    data_points = line_chart.mark_circle().encode(
+        tooltip=['x_values', 'y_values']  # Show x and y values on hover
+    )
+
+    # Combine the line chart and data points
+    chart = (line_chart + data_points).interactive()
+
+    # Display the chart using Streamlit
+    st.altair_chart(chart, use_container_width=True)
+
+if __name__ == '__main__':
+    main()
