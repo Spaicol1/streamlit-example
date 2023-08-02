348e655d55ac2be6d3bd1215b5fe238623844023
Spaicol1 <139074026+Spaicol1@users.noreply.github.com>
2023-08-02 20:59:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,45 +1,38 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+from streamlit_lightweight_charts import renderLightweightCharts
 
-# Sample data for 2020 and 2021, replace with your own data
-data_2020 = pd.DataFrame({
-    'Date': pd.date_range(start='2020-01-01', periods=12, freq='M'),
-    'Value': [10, 12, 15, 8, 10, 11, 14, 17, 20, 21, 19, 18]
-})
+chartOptions = {
+    "layout": {
+        "textColor": 'black',
+        "background": {
+            "type": 'solid',
+            "color": 'white'
+        }
+    }
+}
 
-data_2021 = pd.DataFrame({
-    'Date': pd.date_range(start='2021-01-01', periods=12, freq='M'),
-    'Value': [11, 13, 16, 9, 11, 12, 15, 18, 21, 22, 20, 19]
-})
+seriesLineChart = [{
+    "type": 'Line',
+    "data": [
+        { "time": '2018-12-22', "value": 32.51 },
+        { "time": '2018-12-23', "value": 31.11 },
+        { "time": '2018-12-24', "value": 27.02 },
+        { "time": '2018-12-25', "value": 27.32 },
+        { "time": '2018-12-26', "value": 25.17 },
+        { "time": '2018-12-27', "value": 28.89 },
+        { "time": '2018-12-28', "value": 25.46 },
+        { "time": '2018-12-29', "value": 23.92 },
+        { "time": '2018-12-30', "value": 22.68 },
+        { "time": '2018-12-31', "value": 22.67 },
+    ],
+    "options": {}
+}]
 
-# Combine the datasets
-all_data = {'2020': data_2020, '2021': data_2021}
+st.subheader("Line Chart sample")
 
-def plot_line_chart(data, title):
-    plt.figure(figsize=(10, 6))
-    sns.lineplot(x='Date', y='Value', data=data, marker='o')
-    plt.title(title)
-    plt.xlabel('Date')
-    plt.ylabel('Value')
-    st.pyplot()
-
-def main():
-    st.title('Line Chart with Slider and Filters')
-
-    # Year range slider
-    years = list(all_data.keys())
-    year_range = st.sidebar.slider('Select Year Range', min_value=years[0], max_value=years[-1], value=(years[0], years[-1]))
-
-    # Dataset selection filter
-    selected_dataset = st.sidebar.selectbox('Select Dataset', years)
-
-    # Filter data based on selected year range and dataset
-    filtered_data = pd.concat([all_data[year] for year in years if year_range[0] <= year <= year_range[1]])
-
-    # Plot the line chart
-    plot_line_chart(filtered_data, f'Line Chart for {selected_dataset}')
-
-if __name__ == '__main__':
-    main()
+renderLightweightCharts([
+    {
+        "chart": chartOptions,
+        "series": seriesLineChart
+    }
+], 'line')
