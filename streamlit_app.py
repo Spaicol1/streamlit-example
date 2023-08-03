fc2661b8be51a73dd988504f74f0a6b890b4bc52
Spaicol1 <139074026+Spaicol1@users.noreply.github.com>
2023-08-03 03:02:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,47 @@ import pandas as pd
 import altair as alt
 import numpy as np
 
+# Set Streamlit theme
+st.set_page_config(
+    page_title="My Streamlit App",
+    page_icon="📊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+# Custom Streamlit styles
+st.markdown(
+    """
+    <style>
+    body {
+        font-family: "Arial";
+        color: #333;
+        background-color: #f8f8f8;
+    }
+    .stButton {
+        background-color: #007BFF;
+        color: white;
+        border-radius: 5px;
+        padding: 0.35rem 0.75rem;
+        font-weight: bold;
+        font-size: 14px;
+        cursor: pointer;
+    }
+    .stMetric {
+        background-color: #f0f0f0;
+        color: #333;
+        border-radius: 5px;
+        padding: 0.5rem 0.75rem;
+        text-align: center;
+    }
+    .stMetricDelta {
+        color: #333;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
 def main():
     # Sample data with years from 1900 to 2023 and randomly generated y-values
     years = list(range(1900, 2024))
@@ -22,13 +63,13 @@ def main():
     df = pd.DataFrame(data)
 
     # Create the line chart using Altair
-    line_chart = alt.Chart(df).mark_line().encode(
+    line_chart = alt.Chart(df).mark_line(color='blue').encode(
         x='x_values',
         y='y_values'
     )
 
     # Add interactive data points using the `circle` mark
-    data_points = line_chart.mark_circle().encode(
+    data_points = line_chart.mark_circle(color='orange', size=60).encode(
         tooltip=['x_values', 'y_values']  # Show x and y values on hover
     )
 
@@ -41,7 +82,7 @@ def main():
     highest_value = np.max(y_values)
 
     # Create the average line
-    avg_line = alt.Chart(pd.DataFrame({'avg_value': [avg_value]})).mark_rule(color='orange').encode(y='avg_value')
+    avg_line = alt.Chart(pd.DataFrame({'avg_value': [avg_value]})).mark_rule(color='red').encode(y='avg_value')
 
     # Combine the line chart, data points, and average line using Altair's layer
     chart = alt.layer(line_chart, data_points, avg_line).interactive()
@@ -49,39 +90,41 @@ def main():
     # Add a title to the chart
     st.title("Line Chart with Hover Data Points")
 
-    # Add a header for the page using Markdown
-    st.markdown("## My Streamlit App")
-
     # Add the chart
     st.altair_chart(chart, use_container_width=True)
+
+    # Add a header for the page
+    st.title("My Streamlit App")
 
     # Add metrics for average growth, lowest, highest, and average
     col1, col2, col3, col4 = st.columns(4)
 
     with col1:
-        st.markdown(f"### Average Growth\n\n{avg_growth:.2f}%")
+        st.metric("Average Growth", f"{avg_growth:.2f}%", delta="3.27%")
 
     with col2:
-        st.markdown(f"### Lowest Value\n\n{lowest_value}")
+        st.metric("Lowest Value", lowest_value, delta="-2.12")
 
     with col3:
-        st.markdown(f"### Highest Value\n\n{highest_value}")
+        st.metric("Highest Value", highest_value, delta="4.63")
 
     with col4:
-        st.markdown(f"### Average Value\n\n{avg_value}")
+        st.metric("Average Value", avg_value, delta="1.09")
 
     # Add a sidebar for selecting different sets of data and years
-    selected_establishment_type = st.sidebar.selectbox("Select Establishment Type:", establishment_types)
-    selected_state = st.sidebar.selectbox("Select State:", states)
-    selected_county = st.sidebar.selectbox("Select County:", counties)
-    selected_naics = st.sidebar.selectbox("Select NAICs:", naics)
+    with st.sidebar:
+        st.subheader("Data Selection")
+        selected_establishment_type = st.selectbox("Select Establishment Type:", establishment_types)
+        selected_state = st.selectbox("Select State:", states)
+        selected_county = st.selectbox("Select County:", counties)
+        selected_naics = st.selectbox("Select NAICs:", naics)
 
-    # Add a slider to tweak the years selected
-    start_year = st.sidebar.slider("Select start year:", min_value=min(years), max_value=max(years), value=min(years))
-    end_year = st.sidebar.slider("Select end year:", min_value=min(years), max_value=max(years), value=max(years))
+        st.subheader("Time Range")
+        start_year = st.slider("Select start year:", min_value=min(years), max_value=max(years), value=min(years))
+        end_year = st.slider("Select end year:", min_value=min(years), max_value=max(years), value=max(years))
 
     # Filter the data based on the selected dropdown values and years
-    filtered_data = df  # You can replace this with actual filtering logic based on the selected values and years
+    filtered_data = df[(df['x_values'] >= start_year) & (df['x_values'] <= end_year)]
 
     # Display the filtered data as a table
     st.table(filtered_data)
