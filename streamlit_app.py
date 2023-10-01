7faf6794b669f506387c933c63a742563e3c2119
Spaicol1 <139074026+Spaicol1@users.noreply.github.com>
2023-10-01 16:30:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,37 +13,27 @@ df = pd.DataFrame({
 })
 
 # Sidebar for variable selection
-st.sidebar.title("Variable Selection")
+st.sidebar.title("Map Selection")
 
-year = st.sidebar.slider("Select Year", min_value=df['Year'].min(), max_value=df['Year'].max())
-provincia = st.sidebar.selectbox("Select Provincia", df['Provincia'].unique())
-comune = st.sidebar.selectbox("Select Comune", df[df['Provincia'] == provincia]['Comune'].unique())
-fascia_zona = st.sidebar.selectbox("Select Fascia/Zona", df[(df['Provincia'] == provincia) & (df['Comune'] == comune)]['Fascia_Zona'].unique())
-destinazione_uso = st.sidebar.multiselect("Select Destinazione d'uso", df['Destinazione_Uso'].unique())
+# Create a map with a default view of Italy
+st.sidebar.map([])
 
-# Filter data based on selections
-filtered_df = df[(df['Year'] == year) & (df['Provincia'] == provincia) & (df['Comune'] == comune) & (df['Fascia_Zona'] == fascia_zona) & (df['Destinazione_Uso'].isin(destinazione_uso))]
+# Placeholder to display selected location information
+selected_location = st.sidebar.empty()
 
 # Right side for displaying charts and legend
 st.title("Real Estate Dashboard")
 
-# Display map
-st.write("Map Selection:")
-st.write("You can customize the map display here.")
-
-# Create a map using Streamlit's native st.map function
-st.map(filtered_df)
-
 # Display line chart
 st.write("Line Chart:")
 if st.checkbox("Valore di Mercato $mq"):
-    st.line_chart(filtered_df.groupby('Year')['Valore_Mercato'].mean())
+    st.line_chart(df.groupby('Year')['Valore_Mercato'].mean())
 
 if st.checkbox("Valore Locazione $mq"):
-    st.line_chart(filtered_df.groupby('Year')['Valore_Locazione'].mean())
+    st.line_chart(df.groupby('Year')['Valore_Locazione'].mean())
 
 # Display legend
 st.write("Legend:")
 if st.checkbox("Show Legend"):
-    legend_text = "\n".join([f"{fascia}: {filtered_df[filtered_df['Fascia_Zona'] == fascia]['Valore_Mercato'].mean():.2f}" for fascia in filtered_df['Fascia_Zona'].unique()])
+    legend_text = "\n".join([f"{fascia}: {df[df['Fascia_Zona'] == fascia]['Valore_Mercato'].mean():.2f}" for fascia in df['Fascia_Zona'].unique()])
     st.write(legend_text)
